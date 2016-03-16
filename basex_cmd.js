2e4835194544f82c052c11b28fbf9d29d8994ac8
Peter Scriven <justabitdope@gmail.com>
2016-03-16 19:02:39 +1300

    Small layout changes, added some images, added a besex_cmd file for playing with basex terminal

---
+++ basex_cmd.js
@@ -0,0 +1,14 @@
+client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+               " //name[@type = 'place']",
+  function(error, result) {
+    if(error) {
+      console.error(error);
+    } else {
+      console.log(result.result);
+      console.log("OK  :"+result.ok);
+      console.log("Info:"+result.info.trim());
+      
+      console.log("And thus it was done.");
+    }
+  }
+);
