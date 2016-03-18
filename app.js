84544b7ef1ab470efd6803bd5472aa2c0aac4c4c
Peter SCriven <justabitdope@gmail.com>
2016-03-18 17:12:57 +1300

    Added exsex client action to app.js.

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+// Set up Connection to Server
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+client.execute("OPEN Colenso");
+
 var app = express();
 
 // view engine setup
